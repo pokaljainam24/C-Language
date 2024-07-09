d4d5bbefe2e9b555f58ddb15e6c1d2c39659b506
pokaljainam24 <pokaljainam@gmail.com>
2024-07-09 12:50:42 +0530

    done

---
+++ code.c
@@ -0,0 +1,24 @@
+// #include<stdio.h>
+
+// void main(){
+//     printf("Hello World");
+//     printf("\nJainam POkal");
+// }
+
+// #include<stdio.h>
+
+// void main(){
+//     printf("\n Name:Jainam Pokal");
+//     printf("\n Add: Navsari");
+//     printf("\n Number: 9016883804");
+// }
+
+// #include<stdio.h>
+
+// void main(){
+//     printf("\n*");
+//     printf("\n* *");
+//     printf("\n* * *");
+//     printf("\n* * * *");
+//     printf("\n* * * * *");
+// }
