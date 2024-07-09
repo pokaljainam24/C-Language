0068f5312e1d95cf890a6dc54c5b0694a069e473
pokaljainam24 <pokaljainam@gmail.com>
2024-07-09 14:46:34 +0530

    done

---
+++ Lab-Work/code.c
@@ -0,0 +1,80 @@
+// Q.1 Write a Program to print your name, age, and school.
+
+// #include<stdio.h>
+
+// void main(){
+//     printf("Name: Jainam Pokal \n");
+//     printf("Age: 22 \n");
+//     printf("School: Nasari High School");
+// }
+
+// ********************************************************************************
+
+// Q.2 Write a Program to print the given pattern.
+// - - - - - - - -
+// | |
+// R |
+// N |
+// W |
+// | |
+// - - - - - - - - 
+
+// #include<stdio.h>
+
+// void main(){
+//     printf("- - - - - - - - \n");
+//     printf("|             | \n");
+//     printf("R             | \n");
+//     printf("N             | \n");
+//     printf("W             | \n");
+//     printf("|             | \n");
+//     printf("- - - - - - - - \n");
+// }
+
+// ***********************************************************************************
+
+// Q-3 Write a Program to print the given pattern.
+
+// *
+// * *
+// * * *
+// * *
+// *
+
+// #include<stdio.h>
+
+// void main(){
+//     printf("*\n");
+//     printf("* *\n");
+//     printf("* * *\n");
+//     printf("* *\n");
+//     printf("*\n");
+// }
+
+// ************************************************************************************
+
+// Q-4 Write a Program to print the given pattern.
+
+// *
+// *
+// *
+// *     *  *       *
+// *    *    *     *
+// *   *      *   *
+// *  *        * *
+// * *
+// *
+
+#include<stdio.h>
+
+void main(){
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*      * *       *\n");
+    printf("*     *   *     *\n");
+    printf("*    *     *   *\n");
+    printf("*   *       * *\n");
+    printf("*  *\n");
+    printf("*\n");
+}
