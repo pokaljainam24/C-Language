06b13e205b475b507ffdcf4bd3189f37b6a6e187
pokaljainam24 <pokaljainam@gmail.com>
2024-07-10 12:14:17 +0530

    Work Done

---
+++ L-2(Lab Work)/code.c
@@ -0,0 +1,21 @@
+// #include<stdio.h>
+
+// void main(){
+//     printf("* * * * *\n*\t*\n*\t*\n*\t*\n* * * * *");
+// }
+
+
+#include<stdio.h>
+
+void main(){
+    printf("* * * * *\n * * * *\n  * * *\n   * *\n    *");
+}
+
+
+// #include<stdio.h>
+
+// void main(){
+//     printf("* * * * * * * *\n");
+//     printf("* J A I N A M *\n");
+//     printf("* * * * * * * *\n");
+// }
