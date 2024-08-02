b38d81559dc347f2d6db6e5768778b948d252137
pokaljainam24 <pokaljainam@gmail.com>
2024-08-02 10:32:18 +0530

    work not done

--- L-14(Lab Work #7.2)/code7.c
+++ L-14(Lab Work #7.2)/code7.c
@@ -6,6 +6,26 @@
 // 1 2             2 1
 // 1                 1
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 5;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             printf("%d ", j);   
+//         }
+//         for (int k = i - 1; k >= 1; k--)
+//         {
+//             printf("%d ", k);   
+//         }
+//         printf("\n");
+        
+//     }
+// }
+
 #include <stdio.h>
 
 int main()
