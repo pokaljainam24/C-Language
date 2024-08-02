3cb175d76be84f87e318ac4a750747c9507dad5f
pokaljainam24 <pokaljainam@gmail.com>
2024-08-02 17:58:20 +0530

    work done

--- L-15(Lab Work #7.1)/code1.c
+++ L-15(Lab Work #7.1)/code1.c
@@ -8,7 +8,7 @@ int main()
     {
         for (int j = 0; j <= i; j++)
         {
-            printf("%c", 'A' + i - j);
+            printf("%c ", 'A' + i - j);
         }
         printf("\n");
     }
