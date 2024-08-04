26665bb59d4012131e87801f7901f579346694b6
pokaljainam24 <pokaljainam@gmail.com>
2024-08-04 16:24:26 +0530

    project work done

--- project-4/code1.c
+++ project-4/code1.c
@@ -12,7 +12,7 @@ int main()
     int n = 5, i, j;
     for (i = 0; i <= n; i++)
     {
-        for (j = 0; j <= i; j++)
+        for (j = 0; j <= i - 1; j++)
         {
             printf("%d ",41 + j);
         }
