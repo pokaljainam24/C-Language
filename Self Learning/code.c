94cf3c676d52530e8239e1c434ed5df582365a6a
pokaljainam24 <pokaljainam@gmail.com>
2024-08-12 10:31:40 +0530

    work done

--- Self Learning/code.c
+++ Self Learning/code.c
@@ -173,23 +173,114 @@
 //             6
 // using nested for loop
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i, j, k;
+
+//     for (i = 10; i >= 6; i--)
+//     {
+//         for (j = 10; j > i; j--)
+//         {
+//             printf("  ");
+//         }
+
+//         for (k = i; k >= 6; k--)
+//         {
+//             printf("%d ", k);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// 3. create c program that defines a function to check if a given number is divisible by 3 and 5 both or not.
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     int num;
+//     printf("Enter a number: ");
+//     scanf("%d", &num);
+//     if (num % 3 == 0 && num % 5 == 0)
+//     {
+//         printf("%d is divisible by both 3 and 5", num);
+//     }
+//     else
+//     {
+//         printf("%d is not divisible by both 3 and 5", num);
+//     }
+// }
+
+// 4. write a c program that uses pointers to find the sum of two 1D arrays.
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     int arr1[5] = {1, 2, 3, 4, 5};
+//     int arr2[5] = {6, 7, 8, 9};
+//     int sum = 0;
+//     int *ptr1 = arr1;
+//     int *ptr2 = arr2;
+//     int i;
+//     for (i = 0; i < 5; i++)
+//     {
+//         sum += *ptr1 + *ptr2;
+//         ptr1++;
+//         ptr2++;
+//     }
+//     printf("Sum of two arrays is: %d", sum);
+// }
+
+// 5. develop a c program that uses a structure to represent a mobile (with attributes like company_name,color,model,and price).list N number of mobiles details using Array of objects.
+
 #include <stdio.h>
 
-int main()
-{
-    int i, j, k;
+// Define a structure to represent a mobile
+struct Mobile {
+    char company_name[50];
+    char color[20];
+    char model[50];
+    float price;
+};
 
-    for (i = 10; i >= 6; i--)
-    {
-        for (j = 10; j > i; j--)
-        {
-            printf("  ");
-        }
+int main() {
+    int n;
 
-        for (k = i; k >= 6; k--)
-        {
-            printf("%d ", k);
-        }
-        printf("\n");
+    // Ask the user for the number of mobiles
+    printf("Enter the number of mobiles: ");
+    scanf("%d", &n);
+
+    // Array of structures
+    struct Mobile mobiles[n];
+
+    // Input details for each mobile
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details for mobile %d:\n", i + 1);
+
+        printf("Enter company name: ");
+        scanf("%s", mobiles[i].company_name);
+
+        printf("Enter color: ");
+        scanf("%s", mobiles[i].color);
+
+        printf("Enter model: ");
+        scanf("%s", mobiles[i].model);
+
+        printf("Enter price: ");
+        scanf("%f", &mobiles[i].price);
+    }
+
+    // Display the list of mobiles
+    printf("\nList of Mobile Details:\n");
+    for (int i = 0; i < n; i++) {
+        printf("\nMobile %d:\n", i + 1);
+        printf("Company Name: %s\n", mobiles[i].company_name);
+        printf("Color: %s\n", mobiles[i].color);
+        printf("Model: %s\n", mobiles[i].model);
+        printf("Price: $%.2f\n", mobiles[i].price);
     }
 }
+
